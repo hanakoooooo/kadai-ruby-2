9951cf4991f141d267bade1f24ef29babc5b2d84
hanako.yamada <h.yamada20210929@gmail.com>
2021-10-03 01:36:37 +0000

    add kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,19 @@
+def fizzbuzz
+    
+    (1..100).each do |num|
+    
+     if num % 3 == 0 && num % 5 == 0 
+        puts 'FizzBuzz'
+     elsif num % 3 == 0 
+        puts 'fizz'
+     elsif num % 5 == 0 
+        puts 'Buzz'
+     else puts num
+     end
+     
+     num += 1
+ end
+
+end
+
+fizzbuzz
