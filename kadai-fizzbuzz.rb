7881a0ca9a270447f39932751cb9628c9abe9217
hanako.yamada <h.yamada20210929@gmail.com>
2021-10-03 01:40:20 +0000

    modified kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz
      if num % 3 == 0 && num % 5 == 0 
         puts 'FizzBuzz'
      elsif num % 3 == 0 
-        puts 'fizz'
+        puts 'Fizz'
      elsif num % 5 == 0 
         puts 'Buzz'
      else puts num
